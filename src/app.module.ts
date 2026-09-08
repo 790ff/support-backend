d9cefa0a34390e115b48ab3860aa8ee265c71d35
Thamer Al-Shamrani <alshmranyt071@gmail.com>
2026-09-08 15:27:32 +0300

    feat: integrate TypeORM with PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TicketModule } from './ticket/ticket.module';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, }),
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST', 'localhost'),
+        port: Number(config.get<string>('DB_PORT', '5432')),
+        username: config.get<string>('DB_USERNAME', 'postgres'),
+        password: config.get<string>('DB_PASSWORD', 'postgres'),
+        database: config.get<string>('DB_DATABASE', 'support_db'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
     TicketModule,
   ],
   controllers: [AppController],
