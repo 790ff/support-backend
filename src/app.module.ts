10f3bf114fe3aa19622f383f0aa53f59f5c05990
Thamer Al-Shamrani <alshmranyt071@gmail.com>
2026-09-08 10:45:41 +0300

    Set up modular project structure for tickets

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TicketModule } from './ticket/ticket.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
-    })
+      isGlobal: true, }),
+    TicketModule,
   ],
   controllers: [AppController],
   providers: [AppService],
